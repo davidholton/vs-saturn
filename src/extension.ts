b690d20538fa95980bf54eb4c8456a4725291a40
davidholton <david.holton@outlook.com>
2020-11-11 22:35:56 -0500

    Added strikeText function

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { assert } from 'console';
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // returns string of filled and outlined circles to represent the check marks 
-function generateCheckMarks(completed: number, total: number) {
+function generateCheckMarks(completed: number, total: number): string {
 	return "$(circle-filled)".repeat(completed) + "$(circle-outline)".repeat(total - completed);
+}
+
+// returns input string with a unicode strikethrough char appended after each character 
+function strikeText(text: string): string {
+	return text.split("")
+		.map(char => char + "\u0336")
+		.join("");
 }
 
 // this method is called when your extension is activated
@@ -14,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-saturn" is now active!');
+	console.log("Congratulations, your extension 'vs-saturn' is now active!");
 
 	let statusPriority = 0;
 
@@ -43,13 +49,40 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(vscode.commands.registerCommand('vs-saturn.start', () => {
+	context.subscriptions.push(vscode.commands.registerCommand("vs-saturn.start", () => {
 		// console.log("vs-saturn.start");
 		saturnActionButton.text = saturnActionButton.text === "$(play)" ? "$(debug-pause)": "$(play)";
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('vs-saturn.notes', () => {
-		// console.log("vs-saturn.notes");
+	context.subscriptions.push(vscode.commands.registerCommand("vs-saturn.notes", async () => {
+		console.log("vs-saturn.notes");
+
+		// import { QuickPickItem } from 'vscode';
+		// const quickPickItem: vscode.QuickPickItem = {label: "test"}; // probably not going to use
+		let i = 0;
+
+		let todoList = [
+			strikeText("Walk to get coffee"),
+			"Stretch",
+			"Send a text to Mom"
+		];
+
+		const result = await vscode.window.showQuickPick(todoList, {
+			placeHolder: "Type to add a to-do",
+			onDidSelectItem: item => vscode.window.showInformationMessage(`Focus ${++i}: ${item}`)
+		});
+
+		// const result = await vscode.window.showInputBox({
+		// 	value: "abcdef",
+		// 	valueSelection: [2, 4],
+		// 	placeHolder: "For example: fedcba. But not: 123",
+		// 	validateInput: text => {
+		// 		vscode.window.showInformationMessage(`Validating: ${text}`);
+		// 		return text === "123" ? "Not 123!" : null;
+		// 	}
+		// });
+
+		vscode.window.showInformationMessage(`Got: ${result}`);
 	}));
 
 	context.subscriptions.push(saturnTimerText);
